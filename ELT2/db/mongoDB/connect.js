26ef8f1dabc5abccffc29650c9ae318bbeac3031
amirg00 <amirg00@walla.com>
2022-10-25 01:16:25 +0300

    No need for this package

--- ELT2/db/mongoDB/connect.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('moongose')
-
-const connectionString =
-    "mongodb+srv://yuvalbm:mjRtNevHIdLQPJns@bigdataproject.wztpjci.mongodb.net/bigdataproject?retryWrites=true&w=majority"
-    
-mongoose.connect(connectionString)
